8ff04c37226bf6a3a9295940be288930a6ac5201
Replit user <>
2023-03-17 10:31:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -101,8 +101,8 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
-   if (i >= 0 && i < v->tamano) {
-        return v->elementos[i];
+   if (i >= 0 && i < v->capacidad) {
+        return v->datos[i];
     } else {
         printf("Error: índice fuera de rango\n");
         exit(1);
