54ca5263304c1e4724f1046ca3051961d3ada67f
Replit user <>
2023-03-17 11:41:34 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -114,8 +114,8 @@ Use las operaciones implementadas de vectores para
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-  int *vectorA=NULL;
-  int *vectorB=NULL;
+  Vector *vectorA=NULL;
+  vector *vectorB=NULL;
   vectorA=crearVector(2);
   vectorB=crearVector(2);
   //asignarValor(vector,1,a1);
